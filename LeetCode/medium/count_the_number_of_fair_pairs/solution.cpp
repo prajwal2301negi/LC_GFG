6fb8642c26937ff7541adcc5aa976ce619991c71
Prajwal Negi <166615651+prajwal2301negi@users.noreply.github.com>
2026-06-05 04:48:55 +0530

    Add LeetCode/Count the Number of Fair Pairs

---
+++ LeetCode/medium/count_the_number_of_fair_pairs/solution.cpp
@@ -0,0 +1,25 @@
+// Problem: Count the Number of Fair Pairs
+// Difficulty: medium
+// Runtime: 39 ms
+// Memory: 71.6 MB
+
+class Solution {
+public:
+    long long countFairPairs(vector<int>& nums, int lower, int upper) {
+        sort(nums.begin(), nums.end());
+        return countPairs(nums, upper) - countPairs(nums, lower - 1);
+    }
+    long long countPairs(vector<int>& nums, int target) {
+        long long count = 0;
+        int left = 0, right = nums.size() - 1;
+        
+        while (left < right) {
+            if (nums[left] + nums[right] > target) right--;
+            else {
+                count += right - left;
+                left++;
+            }
+        }
+        return count;
+    }
+};
