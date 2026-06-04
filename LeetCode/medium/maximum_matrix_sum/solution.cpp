3f26d29cb10918315de20e349e34513a7ff86d10
Prajwal Negi <166615651+prajwal2301negi@users.noreply.github.com>
2026-06-05 05:05:53 +0530

    Add LeetCode/Maximum Matrix Sum

---
+++ LeetCode/medium/maximum_matrix_sum/solution.cpp
@@ -0,0 +1,24 @@
+// Problem: Maximum Matrix Sum
+// Difficulty: medium
+// Runtime: 0 ms
+// Memory: 39.3 MB
+
+class Solution {
+public:
+    long long maxMatrixSum(vector<vector<int>>& matrix) {
+        long long totalSum = 0;
+        int neg = 0;
+        int minAbs = INT_MAX;
+
+        for (auto& row : matrix) {
+            for (int v : row) {
+                if (v < 0) neg++;
+                int av = abs(v);
+                totalSum += av;
+                minAbs = min(minAbs, av);
+            }
+        }
+
+        return (neg % 2 == 0) ? totalSum : totalSum - 2LL * minAbs;
+    }
+};
