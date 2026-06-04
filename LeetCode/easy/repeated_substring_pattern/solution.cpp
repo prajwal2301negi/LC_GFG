84e9253bec2f8c16f41c7cf5c2cdc4441711ebd4
Prajwal Negi <166615651+prajwal2301negi@users.noreply.github.com>
2026-06-05 04:52:55 +0530

    Add LeetCode/Repeated Substring Pattern

---
+++ LeetCode/easy/repeated_substring_pattern/solution.cpp
@@ -0,0 +1,31 @@
+// Problem: Repeated Substring Pattern
+// Difficulty: easy
+// Runtime: 45 ms
+// Memory: 94.7 MB
+
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        int n = s.length();
+        for(int l = 1; l <= n/2; l++) {
+            
+            if(n%l == 0) {
+                int times = n/l;
+                
+                string pattern   = s.substr(0, l);
+                string newStr = "";
+                while(times--) {
+                    newStr += pattern;
+                }
+                
+                if(newStr == s)
+                    return true;
+            }
+            
+        }
+        
+        return false;
+    }
+};
+
+
