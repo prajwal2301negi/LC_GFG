0bb8cc5aa595adf2ad46f8310b040b3148c9941d
Prajwal Negi <166615651+prajwal2301negi@users.noreply.github.com>
2026-06-05 05:06:10 +0530

    Add LeetCode/Robot Return to Origin

---
+++ LeetCode/easy/robot_return_to_origin/solution.cpp
@@ -0,0 +1,21 @@
+// Problem: Robot Return to Origin
+// Difficulty: easy
+// Runtime: 3 ms
+// Memory: 10.3 MB
+
+class Solution {
+public:
+
+bool judgeCircle(string moves) {
+    int x = 0, y = 0;
+
+    for (char c : moves) {
+        if (c == 'U') y++;
+        if (c == 'D') y--;
+        if (c == 'R') x++;
+        if (c == 'L') x--;
+    }
+
+    return x == 0 && y == 0;
+}
+};
