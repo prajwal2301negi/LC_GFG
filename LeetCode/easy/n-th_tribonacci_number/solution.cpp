7e46320da541e26225bd179a303610a5b6762d29
Prajwal Negi <166615651+prajwal2301negi@users.noreply.github.com>
2026-06-05 04:51:44 +0530

    Add LeetCode/N-th Tribonacci Number

---
+++ LeetCode/easy/n-th_tribonacci_number/solution.cpp
@@ -0,0 +1,24 @@
+// Problem: N-th Tribonacci Number
+// Difficulty: easy
+// Runtime: 0 ms
+// Memory: 8.3 MB
+
+class Solution {
+public:
+    int tribonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
+
+        vector<int> dp(n + 1);
+
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+
+        return dp[n];
+    }
+};
