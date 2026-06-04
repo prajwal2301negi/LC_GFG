e8d487ffc2561214cd08b4623516349054c5327d
Prajwal Negi <166615651+prajwal2301negi@users.noreply.github.com>
2026-06-05 04:51:15 +0530

    Add LeetCode/Recyclable and Low Fat Products

---
+++ LeetCode/easy/recyclable_and_low_fat_products/solution.cpp
@@ -0,0 +1,7 @@
+// Problem: Recyclable and Low Fat Products
+// Difficulty: easy
+// Runtime: 528 ms
+// Memory: 0.0B
+
+# Write your MySQL query statement below
+select product_id from Products where low_fats=1 AND recyclable=1
