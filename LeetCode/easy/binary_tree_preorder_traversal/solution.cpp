5af2e90d8f81156322fc96b0519d3d27e7f08305
Prajwal Negi <166615651+prajwal2301negi@users.noreply.github.com>
2026-06-05 05:07:31 +0530

    Add LeetCode/Binary Tree Preorder Traversal

---
+++ LeetCode/easy/binary_tree_preorder_traversal/solution.cpp
@@ -0,0 +1,37 @@
+// Problem: Binary Tree Preorder Traversal
+// Difficulty: easy
+// Runtime: 0 ms
+// Memory: 10.7 MB
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if(root == NULL){
+            return ans;
+        }
+        solve(root, ans);
+        return ans;
+    }
+
+    void solve(TreeNode* root, vector<int>& ans){
+        if(root == NULL){
+            return;
+        }
+        ans.push_back(root->val);
+        solve(root->left,ans);
+        solve(root->right, ans);
+
+    }
+};
