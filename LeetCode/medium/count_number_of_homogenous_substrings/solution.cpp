78b1ad8ef859e23aea520c5af30ce418487266f9
Prajwal Negi <166615651+prajwal2301negi@users.noreply.github.com>
2026-06-05 04:54:31 +0530

    Add LeetCode/Count Number of Homogenous Substrings

---
+++ LeetCode/medium/count_number_of_homogenous_substrings/solution.cpp
@@ -0,0 +1,28 @@
+// Problem: Count Number of Homogenous Substrings
+// Difficulty: medium
+// Runtime: 3 ms
+// Memory: 15.1 MB
+
+class Solution {
+public:
+    int MOD = 1e9+7;
+    
+    int countHomogenous(string s) {
+        int n = s.length();
+        int result = 0;
+        int count  = 0;
+        
+        for(int i = 0; i<n; i++) {
+            if(i > 0 && s[i] == s[i-1]) {
+                count++;
+            } else {
+                count = 1;
+            }
+            
+            result = (result + count) % MOD;
+        }
+        
+        return result;
+    }
+};
+
