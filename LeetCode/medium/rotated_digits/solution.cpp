8a26949b1f4d3cf7af3fc24bd908e64139bfc99b
Prajwal Negi <166615651+prajwal2301negi@users.noreply.github.com>
2026-06-05 05:04:19 +0530

    Add LeetCode/Rotated Digits

---
+++ LeetCode/medium/rotated_digits/solution.cpp
@@ -0,0 +1,34 @@
+// Problem: Rotated Digits
+// Difficulty: medium
+// Runtime: 0 ms
+// Memory: 7.9 MB
+
+class Solution {
+public:
+    bool isGood(int num) {
+        bool changed = false;
+
+        while (num > 0) {
+            int digit = num % 10;
+
+            if (digit == 3 || digit == 4 || digit == 7)
+                return false;
+            if (digit == 2 || digit == 5 || digit == 6 || digit == 9)
+                changed = true;
+
+            num /= 10;
+        }
+        return changed;
+    }
+
+    int rotatedDigits(int n) {
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (isGood(i)) {
+                count++;
+            }
+        }
+        return count;
+    }
+};
