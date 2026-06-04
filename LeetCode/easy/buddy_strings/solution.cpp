153f4a3e5a84c252f798d54d408eda10e64262cb
Prajwal Negi <166615651+prajwal2301negi@users.noreply.github.com>
2026-06-05 05:02:36 +0530

    Add LeetCode/Buddy Strings

---
+++ LeetCode/easy/buddy_strings/solution.cpp
@@ -0,0 +1,47 @@
+// Problem: Buddy Strings
+// Difficulty: easy
+// Runtime: 0 ms
+// Memory: 9.2 MB
+
+class Solution {
+public:
+    bool checkFreq(string &s) {
+        int arr[26] = {0};
+        
+        for(char &ch : s) {
+            arr[ch-'a']++;
+            
+            if(arr[ch-'a'] > 1) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    bool buddyStrings(string s, string goal) {
+        if(s.length() != goal.length())
+            return false;
+        
+        if(s == goal) {
+            return checkFreq(s);
+        }
+        
+        vector<int> indices;
+        
+        for(int i = 0; i < s.length(); i++) {
+            if(s[i] != goal[i]) {
+                indices.push_back(i);
+            }
+        }
+        
+        if(indices.size() != 2)
+            return false;
+        
+        int first  = indices[0];
+        int second = indices[1];
+        
+        swap(s[first], s[second]);
+        return s==goal;
+    }
+};
