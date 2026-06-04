eaadf8b023717066c03ac1bbb88b23b60cbaa823
Prajwal Negi <166615651+prajwal2301negi@users.noreply.github.com>
2026-06-05 04:52:00 +0530

    Add LeetCode/Merge Strings Alternately

---
+++ LeetCode/easy/merge_strings_alternately/solution.cpp
@@ -0,0 +1,19 @@
+// Problem: Merge Strings Alternately
+// Difficulty: easy
+// Runtime: 4 ms
+// Memory: 8.5 MB
+
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string result;
+        int i = 0, j = 0;
+        while (i < word1.size() && j < word2.size()) {
+            result += word1[i++];
+            result += word2[j++];
+        }
+        result += word1.substr(i);
+        result += word2.substr(j);
+        return result;
+    }
+};
