63c518312d324b33a927128f184cc4482c1c66e3
Prajwal Negi <166615651+prajwal2301negi@users.noreply.github.com>
2026-06-05 05:06:22 +0530

    Add LeetCode/Words Within Two Edits of Dictionary

---
+++ LeetCode/medium/words_within_two_edits_of_dictionary/solution.cpp
@@ -0,0 +1,30 @@
+// Problem: Words Within Two Edits of Dictionary
+// Difficulty: medium
+// Runtime: 0 ms
+// Memory: 12.6 MB
+
+class Solution {
+public:
+    vector<string> twoEditWords(vector<string>& queries,
+                                vector<string>& dictionary) {
+        int n = queries[0].size();
+        vector<string> ans;
+        for (auto& it : queries) {
+            for (auto& i : dictionary) {
+                int diff = 0;
+                for (int idx = 0; idx < n; idx++) {
+                    if (it[idx] != i[idx]) {
+                        diff++;
+                    }
+                    if (diff > 2)
+                        break;
+                }
+                if (diff <= 2) {
+                    ans.push_back(it);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
