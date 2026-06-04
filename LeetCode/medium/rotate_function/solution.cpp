ec15be09e97a685eeccc8ce1beb03bef8e478732
Prajwal Negi <166615651+prajwal2301negi@users.noreply.github.com>
2026-06-05 05:04:17 +0530

    Add LeetCode/Rotate Function

---
+++ LeetCode/medium/rotate_function/solution.cpp
@@ -0,0 +1,29 @@
+// Problem: Rotate Function
+// Difficulty: medium
+// Runtime: 0 ms
+// Memory: 99.6 MB
+
+class Solution {
+public:
+    int maxRotateFunction(vector<int>& nums) {
+        int n = nums.size();
+     
+
+        int F = 0;
+        int sum = 0;
+
+        for(int i =0;i<n;i++){
+            sum+=nums[i];
+            F+= i*nums[i];
+        }
+
+        int result = F;
+
+        for(int k = 0;k<=n-1;k++){
+            int newF = F+sum-(n*nums[n-1-k]);
+            result = max(result, newF);
+            F=newF;
+        }
+        return result;
+    }
+};
