30634eb9a561b910749282083bfef6ec3d2eb3bb
Prajwal Negi <166615651+prajwal2301negi@users.noreply.github.com>
2026-06-05 05:06:27 +0530

    Add LeetCode/Furthest Point From Origin

---
+++ LeetCode/easy/furthest_point_from_origin/solution.cpp
@@ -0,0 +1,19 @@
+// Problem: Furthest Point From Origin
+// Difficulty: easy
+// Runtime: 0 ms
+// Memory: 9 MB
+
+class Solution {
+public:
+    int furthestDistanceFromOrigin(string moves) {
+        int left = 0, right = 0, blanks = 0;
+
+        for (char c : moves) {
+            if (c == 'L') left++;
+            else if (c == 'R') right++;
+            else blanks++;
+        }
+
+        return abs(left - right) + blanks;
+    }
+};
