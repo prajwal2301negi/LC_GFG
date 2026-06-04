ab0a68d10183e3c80245ff6906f005314b8f62bf
Prajwal Negi <166615651+prajwal2301negi@users.noreply.github.com>
2026-06-05 04:54:40 +0530

    Add LeetCode/Basic Calculator

---
+++ LeetCode/hard/basic_calculator/solution.cpp
@@ -0,0 +1,45 @@
+// Problem: Basic Calculator
+// Difficulty: hard
+// Runtime: 0 ms
+// Memory: 12.4 MB
+
+class Solution {
+public:
+    int calculate(string s) {
+        stack<int> st;
+        long long number = 0;
+        int result = 0;
+        int sign = 1;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (isdigit(s[i])) {
+                number = 10 * number + (s[i] - '0');
+            } else if (s[i] == '+') {
+                result += sign * number;
+                number = 0;
+                sign = 1;
+            } else if (s[i] == '-') {
+                result += sign * number;
+                number = 0;
+                sign = -1;
+            } else if (s[i] == '(') {
+                st.push(result);
+                st.push(sign);
+                result = 0;
+                number = 0;
+                sign = 1;
+            } else if (s[i] == ')') {
+                result += sign * number;
+                number = 0;
+                int top = st.top();
+                st.pop();
+                result *= top;
+                top = st.top();
+                st.pop();
+                result += top;
+            }
+        }
+        result += (sign * number);
+        return result;
+    }
+};
